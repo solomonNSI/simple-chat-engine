40c0de95b6dfa66c7c2d0e931061ad7467bb6118
Suleyman Hanyyev <solomonNSI@users.noreply.github.com>
2023-02-20 14:42:11 +0300

    first commit

---
+++ chatengine.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host="localhost")
+
